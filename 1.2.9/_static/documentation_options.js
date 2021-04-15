f977c1072482e6bfc26a5c35d409af353fab64b8
Documentation Push <nipreps@gmail.com>
2021-04-15 14:20:08 +0000

    doc(update) [skip ci]

--- 1.2.9/_static/documentation_options.js
+++ 1.2.9/_static/documentation_options.js
@@ -1,11 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0rc5+7.g616d2bca',
+    VERSION: '1.3.3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
-    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
--- 1.4.0rc5/api/niworkflows-workflows-epi-refmap-1.py
+++
@@ -1,2 +0,0 @@
-from niworkflows.workflows.epi.refmap import init_epi_reference_wf
-wf = init_epi_reference_wf(omp_nthreads=1)
